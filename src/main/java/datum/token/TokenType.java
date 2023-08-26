5882e5bb2b429c9524e184237fb5b20d31ff7d50
dastan <makkambaevdastan@gmail.com>
2023-08-27 01:27:40 +0600

    Initial commit

---
+++ src/main/java/datum/token/TokenType.java
@@ -0,0 +1,5 @@
+package datum.token;
+
+public enum TokenType {
+  BEARER
+}
---
+++ src/test/java/datum/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package datum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
